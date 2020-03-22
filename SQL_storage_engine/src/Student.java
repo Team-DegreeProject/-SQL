5932fb2cf2090cdc3183632d124064c5e1bc7ccd
foreverF91 <44811315+foreverF91@users.noreply.github.com>
2020-03-22 10:53:41 +0800

    change_json

---
+++ SQL_storage_engine/src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    String name;
+    String id;
+
+    public Student(String n,String i){
+        name = n;
+        id = i;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
