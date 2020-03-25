b5e08835ed749fb6680696949650d8b59d8dd74e
Yuxuan Wang <yuxuanwang0829@163.com>
2020-03-25 19:14:23 +0800

    传输token

--- B+tree/src/implementation/DMLExecution/ExecuteStatement.java
+++
@@ -1,9 +0,0 @@
-package implementation.DMLExecution;
-
-import java.util.List;
-
-public class ExecuteStatement {
-    public static void delete(List tokens){
-        System.out.println("tokens");
-    }
-}
--- B+tree/src/implementation/OurSQLDriver/OurSqlDriverManager.java
+++
@@ -1,6 +0,0 @@
-package implementation.OurSQLDriver;
-//import java.sql.DriverManager;
-
-public class OurSqlDriverManager {
-
-}
