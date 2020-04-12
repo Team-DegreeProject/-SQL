9f4f5a36d76dad7c16b28fc3bb814b6f39c9b708
foreverF91 <44811315+foreverF91@users.noreply.github.com>
2020-04-12 10:14:34 +0800

    4.12

--- OurSql/src/storage/Storage/descriptorSaver.java
+++ OurSql/src/storage/Storage/descriptorSaver.java
@@ -29,23 +29,48 @@ public class descriptorSaver {
         Element tableSchemaElement = new Element("schema").setText(schema);
         Element lockGranularityElement = new Element("lockGranularity").setText(String.valueOf(lockGranularity));
         Document document = new Document(table);
+
+        //将前边所有的element添加到table里边
+        table.addContent(tableNameElement);
+        table.addContent(tableSchemaElement);
+        table.addContent(lockGranularityElement);
+
+
         //获取所有的columnDescriptor
-        for (int i = 0; i < tabledescriptor.getColumnDescriptorList().size(); i++) {
-            ColumnDescriptorList allDescriptor = tabledescriptor.getColumnDescriptorList();
+        ColumnDescriptorList allDescriptor = tabledescriptor.getColumnDescriptorList();
+        for (int i = 0; i < allDescriptor.size(); i++) {
+            //columELement
+            Element columnElement = new Element("columnDescriptors");
+
             //获取每一列的descriptor
             ColumnDescriptor singleColumn = allDescriptor.getColumnDescriptor(i);
             String columnName = singleColumn.getColumnName();
+            Element columnNameElement = new Element("columnName").setText(columnName);
+
             int columnPosition = singleColumn.getPosition();
+            Element columnPositionElement = new Element("columnPosition").setText(String.valueOf(columnPosition));
 
             //dataTypedescriptor里边的所有的属性
             DataTypeDescriptor typeDescriptor = singleColumn.getType();
+            Element DataTypeDescriptorElement = new Element("DataTypeDescriptor");
+
             int typeId = typeDescriptor.getTypeId();
+            Element typeIdElement = new Element("typeId").setText(String.valueOf(typeId));
+
             int precision= typeDescriptor.getPrecision();
+            Element precisionElement = new Element("precision").setText(String.valueOf(precision));
+
             int scale= typeDescriptor.getScale();
+            Element scaleElement = new Element("scale").setText(String.valueOf(scale));
+
             boolean isNullable= typeDescriptor.isNullable();
+            Element isNullableElement = new Element("isNullable").setText(String.valueOf(isNullable));
+
             boolean primaryKey= typeDescriptor.isPrimaryKey();
+            Element primaryKeyElement = new Element("primaryKey").setText(String.valueOf(primaryKey));
 
             long autoincStart = singleColumn.getAutoincStart();
+            
             long autoincInc = singleColumn.getAutoincInc();
             long autoincValue = singleColumn.getAutoincValue();
             boolean autoincCycle = singleColumn.getAutoincCycle();
